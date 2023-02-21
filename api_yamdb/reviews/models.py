7c384f0aca3a5cd2a9c306c26652fd0077715d9e
Илья Василевский <vasilevskijila047@gmail.com>
2023-02-21 20:31:13 +0300

    Добавил 3 модели Category, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,36 @@
+from datetime import date
 from django.contrib.auth import get_user_model
 from django.db import models
 
 User = get_user_model()
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.slug
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50)
+
+    def __str__(self):
+        return self.slug
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.DateField(default=date.year)
+    # rating = models.ForeignKey()
+    description = models.TextField()
+    genre = models.ForeignKey(
+        Genre,
+        on_delete=models.CASCADE,
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+    )
