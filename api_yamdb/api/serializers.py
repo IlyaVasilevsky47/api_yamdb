31444437d256956f27500e3ad3b8e933b889e8a9
Илья Василевский <vasilevskijila047@gmail.com>
2023-02-21 22:07:59 +0300

    Добавил модель GenreTitle для связи многоим ко многоим, в модели Title в строке genre.

---
+++ api_yamdb/api/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+
+from reviews.models import Category, Genre, Title
+
+
+class CatehorySerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Genre
+
+
+class TitleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = '__all__'
+        model = Title
