d50ba32a22d031a831d42c7bf2c331a1c54b2263
Игорь Белошицкий <i98el@yandex.ru>
2023-02-23 01:04:17 +0300

    added models Review & Comment

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 User = get_user_model()
 
@@ -41,3 +42,62 @@ class GenreTitle(models.Model):
 
     def __str__(self):
         return self.slug
+
+
+class Review(models.Model):
+    text = models.TextField(
+        verbose_name="Отзыв",
+        help_text="Отзыв о произведениии"
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата публикации",
+        help_text="Дата публикации отзыва"
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        verbose_name="Автор",
+        help_text="Автор отзыва"
+    )
+    score = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ],
+        verbose_name="Оценка",
+        help_text="Оцените произведение от 1 до 10"
+    )
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(
+        Review,
+        related_name='comments',
+        on_delete=models.CASCADE,
+        verbose_name="Комментируемое произведение",
+        help_text="Оставте свой комментарий"
+    )
+    author = models.ForeignKey(
+        User,
+        related_name='comments',
+        on_delete=models.CASCADE,
+        verbose_name="Автор",
+        help_text="Автор комментария"
+    )
+    text = models.TextField(
+        verbose_name="Комментарий",
+        help_text="Текст комментария"
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Дата публикации комментария",
+        help_text="Дата публикации комментария"
+    )
+
+    def __str__(self):
+        return self.text
