f8141a8efae9cdf2e0f367123c6c3e0f9bd0ad2c
Илья Василевский <vasilevskijila047@gmail.com>
2023-02-23 23:21:17 +0300

    Изменил в модели title поле year, добавил фильтрацию и доделал запросы.

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,32 +1,37 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
+from rest_framework import filters
 
 from reviews.models import Category, Genre, Title
 
 from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
 
 
-# Запросы GET, POST, DEL
-# Фильтр:
-# search, string - Поиск по названию категории
-class CategoryViewSet(viewsets.ModelViewSet):
+class ListCreateDestroy(mixins.ListModelMixin,
+                        mixins.CreateModelMixin,
+                        mixins.DestroyModelMixin,
+                        viewsets.GenericViewSet):
+    pass
+
+
+class CategoryViewSet(ListCreateDestroy):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    # permission_classes = ('',)
+    filter_bckends = (filters.SearchFilter, )
+    search_fields = ('name',)
 
 
-# Запросы GET, POST
-# Фильтр:
-# search, string - Поиск по названию жанра
-class GenreViewSet(viewsets.ModelViewSet):
+class GenreViewSet(ListCreateDestroy):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
+    # permission_classes = ('',)
+    filter_bckends = (filters.SearchFilter, )
+    search_fields = ('name',)
 
 
-# Запросы GET, POST, id_title запросы к GET, POST, PATCH, DEL
-# Фильтр:
-# 1. category, string - фильтрует по полю slug категории
-# 2. genre, string - фильтрует по полю slug жанра
-# 3. name, string - фильтрует по названию произведения
-# 4. year, integer - фильтрует по году
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
+    # permission_classes = ('',)
+    filter_bckends = (filters.SearchFilter, )
+    search_fields = ('category', 'genre', 'name', 'year')
