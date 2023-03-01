93049026485ac4c0408a149ff417aa19debf6af7
Angelina Tingaeva <angelinatingaeva@yandex.ru>
2023-03-02 00:27:26 +0500

    изменено: в ReviewUserViewSet добавлен lookup_field по username и lookup_value_regex по регулярному выражению

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -5,7 +5,7 @@ from django.core.mail import send_mail
 from api_yamdb.settings import CONFIRMATION_CODE_LEN, CONTACT_EMAIL
 
 
-def send_confirmation_code(email: str, confirmation_code: str) -> None:
+def send_confirmation_code(email, confirmation_code):
     """Отправляет код подтверждения на почту пользователя."""
     send_mail(
         subject='Код подтверждения',
